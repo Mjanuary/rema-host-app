1b7d872df1ea55641ee5558d60ab7272a345e9ba
Mjanuary <janviermuhawenimana@gmail.com>
2020-07-24 14:09:58 +0200

    Updates

---
+++ precache-manifest.7ffbb0d3ebd06dd0bbe7b43e1a54707b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957f1d30648bd81b8d137974f9d8a521",
+    "url": "/rema-host-app/index.html"
+  },
+  {
+    "revision": "7383e07dd8e54e3fd879",
+    "url": "/rema-host-app/static/css/main.7efb821b.chunk.css"
+  },
+  {
+    "revision": "fe3b01ed0c09850a6b9e",
+    "url": "/rema-host-app/static/js/2.5ac01835.chunk.js"
+  },
+  {
+    "revision": "2e4f31804d72137a9d9b13db1a0e4557",
+    "url": "/rema-host-app/static/js/2.5ac01835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7383e07dd8e54e3fd879",
+    "url": "/rema-host-app/static/js/main.56c52848.chunk.js"
+  },
+  {
+    "revision": "c1c58b1bdeeec8941e1e",
+    "url": "/rema-host-app/static/js/runtime-main.35623f36.js"
+  },
+  {
+    "revision": "2a5b7b2ef1ad471e22a4ad703f55ad7a",
+    "url": "/rema-host-app/static/media/Administrative and Geographical Characteristics of NAKU.2a5b7b2e.png"
+  },
+  {
+    "revision": "68589e54f7e2663f9655cd956aaf55c1",
+    "url": "/rema-host-app/static/media/Existing Soil Control Mechanisms.68589e54.png"
+  },
+  {
+    "revision": "cd4e25ba0fe72cef618993c418730819",
+    "url": "/rema-host-app/static/media/Flood and Landslide Hazards Risk.cd4e25ba.png"
+  },
+  {
+    "revision": "ffa302ed3fa95dca2829b39bfbb2895c",
+    "url": "/rema-host-app/static/media/Geology.ffa302ed.png"
+  },
+  {
+    "revision": "b8b7dfc567e60ee808a0e7e2d71b528f",
+    "url": "/rema-host-app/static/media/Hydrographic Characteristics.b8b7dfc5.png"
+  },
+  {
+    "revision": "956918ea126f6d7d58cd89ffaca3af49",
+    "url": "/rema-host-app/static/media/Location of the catchments.956918ea.png"
+  },
+  {
+    "revision": "253e45f4734a270c4fd6d1b2ad63da3e",
+    "url": "/rema-host-app/static/media/rema_icon.253e45f4.png"
+  }
+]);
